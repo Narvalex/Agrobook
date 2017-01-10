d291368bf3687ed6fe7f668f8ca73248b06e5fd1
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-01-10 15:27:45 -0300

    Added the server project

--- src/Agrobook.Server/Program.cs
+++ src/Agrobook.Server/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Agrobook.Core
+namespace Agrobook.Server
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
