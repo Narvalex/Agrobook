8e8dd5468b3c9c90964dfe2db168490268f241af
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-01-23 15:17:01 -0300

    playing with theming

--- src/Agrobook.Web/app/dist/boot.js
+++ src/Agrobook.Web/app/dist/boot.js
@@ -1,8 +1,14 @@
 /// <reference path="_all.ts" />
 var ContactManagerApp;
 (function (ContactManagerApp) {
-    angular.module("contactManagerApp", ['ngMaterial'])
+    angular.module("contactManagerApp", ['ngMaterial', 'ngMdIcons'])
         .service('userService', ContactManagerApp.UserService)
-        .controller("mainController", ContactManagerApp.MainController);
+        .controller("mainController", ContactManagerApp.MainController)
+        .config(function ($mdIconProvider, $mdThemingProvider) {
+        $mdIconProvider.icon('menu', './assets/svg/menu.svg', 24);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
+    });
 })(ContactManagerApp || (ContactManagerApp = {}));
 //# sourceMappingURL=boot.js.map
--- src/Agrobook.Web/app/src/boot.ts
+++ src/Agrobook.Web/app/src/boot.ts
@@ -1,7 +1,17 @@
 ﻿/// <reference path="_all.ts" />
 
 module ContactManagerApp {
-    angular.module("contactManagerApp", ['ngMaterial'])
+    angular.module("contactManagerApp", ['ngMaterial', 'ngMdIcons'])
         .service('userService', UserService)
-        .controller("mainController", MainController);
+        .controller("mainController", MainController)
+        .config((
+            $mdIconProvider: angular.material.IIconProvider,
+            $mdThemingProvider: angular.material.IThemingProvider) => {
+
+            $mdIconProvider.icon('menu', './assets/svg/menu.svg', 24);
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue')
+                .accentPalette('red');
+        });
 }
