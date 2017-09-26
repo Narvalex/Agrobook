765f9445d5bcabe640f71a9c98373648ff85147c
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-09-26 16:45:19 -0400

    Added categories queries

---
+++ src/CategoriesProjection.js
@@ -0,0 +1,31 @@
+fromStream('$streams')
+.when({
+    $init: function () {
+        return []
+    },
+    $any: function (s, e) {
+        var category = e.streamId.split("-")[0];
+
+        if (s.includes(category))
+            return;
+
+        s.push(category);
+    }
+})
+
+//fromStream('$streams')
+fromAll()
+    .when({
+        $init: function () {
+            return { eventCount: 0, categories: [] }
+        },
+        $any: function (s, e) {
+            s.eventCount++;
+            var category = e.streamId.split("-")[0];
+
+            if (s.categories.includes(category))
+                return;
+
+            s.categories.push(category);
+        }
+    })
