e8c23f84578ee628b13feccbc66795d8e33c1113
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-10-31 16:18:37 -0300

    Este codigo no compilar. Im sorry im perry.

---
+++ src/Eventing.Core/Log/ILogLite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Eventing.Log
+{
+    public interface ILogLite
+    {
+        void Error(string message);
+        void Error(Exception ex, string message);
+        void Fatal(string message);
+        void Fatal(Exception ex, string message);
+        void Info(string message);
+        void Verbose(string message);
+        void Warning(string message);
+        void Success(string message);
+    }
+}
---
+++ src/Eventing.Core/Log/LogMessageBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Eventing.Log
+{
+    /// <summary>
+    /// A log message builder
+    /// </summary>
+    public class LogMessageBuilder
+    {
+        private static readonly int _processId = Process.GetCurrentProcess().Id;
+        private readonly string componentName;
+
+        public LogMessageBuilder(string componentName)
+        {
+            Ensure.NotNullOrWhiteSpace(componentName, nameof(componentName));
+
+            this.componentName = componentName;
+        }
+
+        public string BuildMessage(string level, string format, params object[] args)
+        {
+            return string.Format("[{0:00000},{1:00} {2:dd/MM/yy HH:mm:ss.fff} {3} {4}] {5}",
+                                    _processId,
+                                    Thread.CurrentThread.ManagedThreadId,
+                                    DateTime.Now,
+                                    this.componentName,
+                                    level,
+                                    args.Length == 0 ? format : string.Format(format, args));
+        }
+
+        public string BuildMessage(Exception ex, string level, string format, params object[] args)
+        {
+            var stringBuilder = new StringBuilder();
+            while (ex != null)
+            {
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine(ex.ToString());
+                ex = ex.InnerException;
+            }
+
+            return string.Format("[{0:dd/MM/yy HH:mm:ss.fff} {1} {2}] {3}\nEXCEPTION(S) OCCURRED:{4}",
+                                 DateTime.Now,
+                                 this.componentName,
+                                 level,
+                                 args.Length == 0 ? format : string.Format(format, args),
+                                 stringBuilder);
+        }
+    }
+}
